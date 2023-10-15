73653a23a1fe11fc3212a9bb66411235195ab3ed
Ha Minh Hieu <hieuu02003@gmail.com>
2023-10-15 13:48:58 +0700

    Tu connect

--- ConnectSQL/src/connectsql/ConnectSQL.java
+++ ConnectSQL/src/connectsql/ConnectSQL.java
@@ -9,44 +9,61 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
 
 /**
  *
  * @author minhius23
  */
 public class ConnectSQL {
-    
-
+ 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+       
+       
         String connectionUrl
                 = "jdbc:sqlserver://PC-minhius\\HAMINHHIEU:1433;"
                 + "database=quanlisinhvien;"
                 + "user=sa;"
                 + "password=123456;"
                 + "encrypt=true;"
-                + "trustServerCertificate=true;"
-                + "loginTimeout=30;";
-        try (Connection conn = DriverManager.getConnection(connectionUrl);) {
-            Statement stm = conn.createStatement();
-            String sql = "select * from Sinhvien";
-            ResultSet rs = stm.executeQuery(sql);
-            while (rs.next()) {
-                System.out.println("MSV " + rs.getString(1));
-                System.out.println("Ten " + rs.getString(2));
-                System.out.println("Ngaysinh " + rs.getString(3));
-                System.out.println("Gioi tinh " + rs.getString(4));
-                System.out.println("Malop " + rs.getString(5));
-                System.out.println("Tenlop " + rs.getString(6));
-                System.out.println("==========================================");
-
-            }
-        } // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            System.out.print("ERROR");
-        }
+                + "trustServerCertificate=true;";
+               
+         Connection conn = DriverManager.getConnection(connectionUrl);
     }
+   public Vector<Vector<String>> getAll() throws SQLException {
+	Vector<Vector<String>> data = new Vector<>();
 
+	// Kết nối database
+	connection = getConnect();
+
+	// Tạo câu lệnh SQL (Cách 1: dùng Statement)
+	Statement stmt = connection.createStatement();
+	ResultSet rs = stmt.executeQuery("Select * from Students");
+	while (rs.next()) {
+
+	    // Lấy dữ liệu từ ResultSet
+	    String Sno = rs.getString(1);
+	    String Sname = rs.getString(2);
+	    String Sgender = rs.getString(3);
+	    String Sclass = rs.getString(4);
+	    String Saddress = rs.getString(5);
+	    String Semail = rs.getString(6);
+
+	    // Ghi vào vector
+	    Vector<String> temp = new Vector<>();
+	    temp.add(Sno);
+	    temp.add(Sname);
+	    temp.add(Sgender);
+	    temp.add(Sclass);
+	    temp.add(Saddress);
+	    temp.add(Semail);
+
+	    // Thêm dữ liệu vào data vector chính
+	    data.add(temp);
+	}
+	return data;
+    }
 }
--- ConnectSQL/src/connectsql/Form9Button.java
+++ ConnectSQL/src/connectsql/Form9Button.java
@@ -1,18 +1,18 @@
+package connectsql;
 
+
+import connectsql.ConnectSQL;
 /**
  *
  * @author minhius23
  */
 
-public class Form9Button extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Form9Button
-     */
+public class Form9Button extends javax.swing.JFrame {
     public Form9Button() {
         initComponents();
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -145,14 +145,15 @@ public class Form9Button extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-       
+           System.out.println();
+           
       
        
         java.awt.EventQueue.invokeLater(new Runnable() {
